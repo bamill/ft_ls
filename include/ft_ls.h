a8036984138bc45911135d66be62b870b550b30b
Bret MILLER <abunai@sashizu.me>
2017-05-20 18:23:23 -0700

    initial

---
+++ include/ft_ls.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiller <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/20 18:00:37 by bmiller           #+#    #+#             */
+/*   Updated: 2017/05/20 18:19:09 by bmiller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+# include <unistd.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <termios.h>
+# include "libft.h"
+
+#endif
